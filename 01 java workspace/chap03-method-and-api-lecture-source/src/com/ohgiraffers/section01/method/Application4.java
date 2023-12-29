1a10bf4344c1b94147088ec6fc1cc496eb79d1ee
ddalla0425 <hiwgod92@gmail.com>
2023-12-29 17:29:03 +0900

    first commit

---
+++ 01 java workspace/chap03-method-and-api-lecture-source/src/com/ohgiraffers/section01/method/Application4.java
@@ -0,0 +1,24 @@
+package com.ohgiraffers.section01.method;
+
+public class Application4 {
+    public static void main(String[] args) {
+        //여러 개의 전달 인자를 이용한 메소드 호출을 할 수 있다.
+        Application4 app4 = new Application4();
+        app4.testMethod("이장님",62,'남');
+       // app4.testMethod(62,"이장님",'남'); 오류뜸/ 아규먼트 순서를 지키지 않았기 때문, 아규먼트 수가 모자라도 오류뜸
+
+        //변수에 저장 된 값을 전달하며 호출
+        String name = "유관순";
+        int age = 20;
+        char gender ='여';
+
+        app4.testMethod(name, age, gender);
+    }
+    public void testMethod(String name, int age, final char gender) {
+        //매개변수도 일종의 지역 변수로 분류된다. final 키워드도 사용 가능하다.
+        //단, final 매개변수는 상수 네이밍 규칙을 선택적으로 따르는 경향이 있다.
+        System.out.println("당신의 이름은 " + name + "이고, 나이는 " + age + "세 이며, 성별은 " + gender + "입니다.");
+
+
+    }
+}
